fee51dbd0665dca06c7d99b2cc78abc971c37da9
username-shakha <username.shakha@gmail.com>
2024-03-28 13:52:25 +0500

    create icons component

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,25 @@
+export const DownIcon = () => (
+    <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line x1="0.353553" y1="0.646447" x2="6.18011" y2="6.47301" stroke="black" />
+        <line x1="5.64645" y1="6.30331" x2="11.3033" y2="0.646453" stroke="black" />
+    </svg>
+)
+
+export const UpIcon = () => (
+    <svg width="12" height="7" viewBox="0 0 12 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <line
+            y1="-0.5"
+            x2="8.24"
+            y2="-0.5"
+            transform="matrix(0.707107 -0.707107 -0.707107 -0.707107 0 5.82655)"
+            stroke="black"
+        />
+        <line
+            y1="-0.5"
+            x2="8"
+            y2="-0.5"
+            transform="matrix(0.707107 0.707107 0.707107 -0.707107 6 0.169693)"
+            stroke="black"
+        />
+    </svg>
+)
