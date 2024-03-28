f14790a2e98c19253bcad06350206242079a8123
username-shakha <username.shakha@gmail.com>
2024-03-28 14:56:33 +0500

    autocomplete create props

--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -1,17 +1,48 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import './style.sass'
 
-function Autocomplete() {
+interface IAutocompleteProps {
+    options: string[]
+    title: string
+}
+
+function Autocomplete({ options, title }: IAutocompleteProps) {
     const [inputValue, setInputValue] = useState('')
-    console.log(inputValue)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    useEffect(() => {
+        const handleMouseDown = () => {
+            setIsOpen(false)
+        }
+
+        document.addEventListener('mousedown', handleMouseDown)
+
+        return () => {
+            document.removeEventListener('mousedown', handleMouseDown)
+        }
+    }, [])
+
     return (
         <div className="autocomplete">
-            <label htmlFor="">label</label>
+            <label htmlFor="autocomplete">{title ? title : 'Выберите параметры'}</label>
             <input
                 type="text"
-                placeholder="input"
+                placeholder="Select Option"
+                id="autocomplete"
                 value={inputValue}
                 onChange={({ target }) => setInputValue(target.value)}
+                onFocus={() => setIsOpen(true)}
+                onMouseDown={(e) => e.stopPropagation()}
             />
+            {isOpen && (
+                <ul className="options">
+                    <li>List Option</li>
+                    <li>List Option 1</li>
+                    <li>List Option 2</li>
+                    <li>List Option 3</li>
+                    <li>List Option 4</li>
+                </ul>
+            )}
         </div>
     )
 }
